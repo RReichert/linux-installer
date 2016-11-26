c05ccfe6211dceedba451e35273c8f8f9007a698
Rodrigo Reichert <reichert.rodrigo@gmail.com>
2016-11-27 00:15:51 +1030

    trimmed unnesisary packages

--- install-dnf.sh
+++ install-dnf.sh
@@ -22,68 +22,29 @@ sudo -E dnf -y upgrade
 
 # install packages
 sudo -E dnf -y install \
-	alsa-lib-devel \
-	armadillo-devel \
-	automake \
-	banshee \
-	bison \
 	boost \
 	boost-devel \
 	cmake \
-	cups-devel \
-	curl \
-	dbus \
-	dbus-devel \
 	docker \
 	doxygen \
-	eclipse-jdt \
-	flex \
-	fontconfig-devel \
 	gcc \
 	gcc-c++ \
 	gimp \
 	git \
 	google-chrome-stable \
-	gperf \
-	graphviz \
 	inotify-tools \
 	kernel-devel \
-	libcap-devel \
-	libgcrypt \
-	libgcrypt-devel \
-	libgudev1-devel \
-	libicu-devel \
-	libX11-devel \
-	libxcb \
-	libxcb-devel \
-	libXcomposite-devel \
-	libXcursor-devel \
-	libXi-devel \
-	libXrandr-devel \
-	libXrender-devel \
-	libxslt-devel \
-	libXtst-devel \
 	make \
-	mysql-dev \
 	moreutils \
-	nss-devel \
 	openssh-server \
-	p7zip \
-	pciutils-devel \
-	postgresql-devel \
-	pulseaudio-libs-devel \
 	socat \
 	swig \
-	systemd-devel \
 	valgrind \
 	vim-enhanced \
 	VirtualBox \
 	vlc \
-	xcb-util \
-	"xcb-util-*-devel" \
-	xcb-util-devel \
 	xmlstarlet
 
 # install codecs
-#sudo dnf install gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free-extras ffmpeg
+sudo dnf install gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free-extras ffmpeg gstreamer1-libav gstreamer1-plugin-openh264 gstreamer1-plugin-mpg123
 
